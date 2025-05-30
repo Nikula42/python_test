f19dce4acb3ba9d10ec8914e9d675736387c9b46
Никита <ngnikulnikov@gmail.com>
2025-05-30 14:40:54 +0300

    релиз

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
+import os
 import os.path
 
-from flask import Flask, render_template, request, redirect, flash
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
+from flask_login import UserMixin
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.id = username
+        self.username = username
+        self.password = password
+
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.secret_key = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATA_URL')
+app.config['UPLOAD_FOLDER'] = os.getenv('STATIC')
 db = SQLAlchemy(app)
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+users = {}
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get(user_id)
 
 
 class Post(db.Model):
@@ -22,6 +48,7 @@ class Post(db.Model):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/')
 def index():
     posts = Post.query.all()
@@ -33,7 +60,9 @@ def hello(id):
     post2 = Post.query.get_or_404(id)
     return render_template('post.html', post=post2)
 
+
 @app.route('/add_post', methods=["POST", "GET"])
+@login_required
 def add_page():
     if request.method == 'POST':
         title = request.form['text_web']
@@ -49,7 +78,6 @@ def add_page():
             photo.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         elif photo:
             flash('Не подходящий формат файла')
-
         try:
             new_post = Post(title=title, text=text, photo_path=photo_path, category=category)
             db.session.add(new_post)
@@ -61,9 +89,49 @@ def add_page():
     else:
         return render_template("add post.html")
 
+
 @app.route('/information')
 def info():
     return render_template('info.html')
+
+
+@app.route('/auth/register', methods=['GET', 'POST'])
+def register():
+    global next_user_id
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users:
+            print("Пользователь с таким именем уже существует.")
+            return redirect(url_for('register'))
+
+        user = User(username, password)
+        users[username] = user
+
+        print("Вы успешно зарегистрированы!")
+        print(users)
+        return redirect(url_for('index'))
+    return render_template('register.html')
+
+
+@app.route('/auth/login', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = users.get(username)
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('index'))
+        return 'Неверные учетные данные'
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
